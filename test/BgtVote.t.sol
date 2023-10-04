2132df88d0371f390f9ba7479e651ad23b8855a6
viet tu <quyen.luminous@gmail.com>
2023-10-04 18:41:55 +0700

    (+) merkle proof

--- test/BgtVote.t.sol
+++ test/BgtVote.t.sol
@@ -16,6 +16,8 @@ contract BgtVoteTest is Test {
     address public user3 = vm.addr(user3PKey);
     uint256 public user4PKey = 0x4;
     address public user4 = vm.addr(user4PKey);
+    uint256 public user5PKey = 0x5;
+    address public user5 = vm.addr(user5PKey);
 
     function setUp() public {
         bgt = new BeinGiveTake();
@@ -44,9 +46,9 @@ contract BgtVoteTest is Test {
         uint256[] memory values = new uint256[](1);
         values[0] = 0;
         bytes[] memory callDatas = new bytes[](1);
-        callDatas[0] = abi.encodeWithSignature("mintTo(address,uint256)", user1, 100);
+        callDatas[0] = abi.encodeWithSignature("mintTo(address,uint256)", user5, 100);
         vm.prank(user1);
-        uint256 proposeId = bgtVote.propose(targets, values, callDatas, "Lets mint 100 BGT for user1");
+        uint256 proposeId = bgtVote.propose(targets, values, callDatas, "Lets mint 100 BGT for user5");
         assertEq(uint(bgtVote.state(proposeId)), 0);
 
         vm.prank(user2);
@@ -59,14 +61,26 @@ contract BgtVoteTest is Test {
         vm.roll(block.number + 5 + 1);
         assertEq(uint(bgtVote.state(proposeId)), 1);
 
+        assertEq(bgtVote.getVotes(user1), 3000);
+
         vm.prank(user1);
         bgtVote.castVote(proposeId, 1);
         vm.prank(user2);
-        bgtVote.castVote(proposeId, 1);
+        bgtVote.castVote(proposeId, 1);  // no effect
         vm.prank(user3);
         bgtVote.castVote(proposeId, 1);
         vm.prank(user4);
         bgtVote.castVote(proposeId, 0);
+
+        vm.roll(block.number + 1);
+
+        assertEq(bgtVote.getVotes(user1), 3000);
+        bgt.mintTo(user1, 100);
+        bgt.mintTo(user2, 200);
+
+        vm.roll(block.number + 1);
+
+        assertEq(bgtVote.getVotes(user1), 3000);
 
         (uint256 againstVotes, uint256 forVotes, uint256 abstainVotes) = bgtVote.proposalVotes(proposeId);
         assertEq(againstVotes, 4000);
@@ -77,8 +91,8 @@ contract BgtVoteTest is Test {
         assertEq(uint(bgtVote.state(proposeId)), 4); // succeeded
 
         vm.prank(user1);
-        bgtVote.execute(targets, values, callDatas, keccak256(bytes("Lets mint 100 BGT for user1")));
-        assertEq(bgt.balanceOf(user1), 1100);
+        bgtVote.execute(targets, values, callDatas, keccak256(bytes("Lets mint 100 BGT for user5")));
+        assertEq(bgt.balanceOf(user5), 100);
     }
 
 }
---
+++ test/RewardsDistributor.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "../src/token/TokenVote.sol";
+import "forge-std/Test.sol";
+import "../src/reward/RewardDistributior.sol";
+
+contract RewardsDistributorTest is Test {
+    TokenVote public token;
+    RewardsDistributor public rewardsDistributor;
+    uint256 public user1PKey = 0x1;
+    address public user1 = vm.addr(user1PKey);
+    uint256 public user2PKey = 0x2;
+    address public user2 = vm.addr(user2PKey);
+    uint256 public user3PKey = 0x3;
+    address public user3 = vm.addr(user3PKey);
+    uint256 public user4PKey = 0x4;
+    address public user4 = vm.addr(user4PKey);
+    uint256 public user5PKey = 0x5;
+    address public user5 = vm.addr(user5PKey);
+
+    function setUp() public {
+        token = new TokenVote();
+        rewardsDistributor = new RewardsDistributor(address(token), 0x0);
+
+        token.mintTo(address(rewardsDistributor), 1000);
+    }
+
+    function testClaim() public {
+        bytes32[] memory merkleProof = new bytes32[](1);
+        merkleProof[0] = bytes32(0x0);
+        vm.prank(user1);
+        rewardsDistributor.claim(0, user1, 100, merkleProof);
+        uint256 user1Votes = token.balanceOf(user1);
+        assertEq(user1Votes, 100);
+    }
+}
